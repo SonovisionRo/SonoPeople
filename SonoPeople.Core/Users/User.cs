1152e55a441907849a79cb099e4cc145b822e04e
ionutcorneanu <ionut_corneanu@yahoo.com>
2016-07-25 15:24:07 +0300

    remove ignored files

--- SonoPeople.Core/Users/User.cs
+++ SonoPeople.Core/Users/User.cs
@@ -8,15 +8,14 @@ using System.Threading.Tasks;
 namespace SonoPeople.Core.Users
 {
     public class User : EntityBase<int>
-    {
-        public int Id { get; set; }
+    {     
 
         public string Email { get; set; }
         public bool IsActivated { get; set; }
         public DateTime CreatedOn { get; set; }        
         protected override void Validate()
         {
-            if
+           
         }
     }
 }
