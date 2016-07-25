875601e4c2693c3f141e0f5560fbdfb1629b9722
ionutcorneanu <ionut_corneanu@yahoo.com>
2016-07-25 14:34:00 +0300

    IASIDIVERS-153 User Model

---
+++ SonoPeople.Core/Users/User.cs
@@ -0,0 +1,22 @@
+﻿using SonoPeople.Infrastructure.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonoPeople.Core.Users
+{
+    public class User : EntityBase<int>
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+        public bool IsActivated { get; set; }
+        public DateTime CreatedOn { get; set; }        
+        protected override void Validate()
+        {
+            if
+        }
+    }
+}
